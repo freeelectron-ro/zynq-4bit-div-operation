e2492bbe1dc36da6991b4220c45ddbf9d7565264
freeelectron-ro <freeelectron-ro@users.noreply.github.com>
2019-12-30 19:08:52 +0200

    Update top.v
    corrected comments

--- top.v
+++ top.v
@@ -20,9 +20,9 @@
 //  This small sketch demonstrates the division operation on Zybo board.
 //  This was implemented to use only the PL, but no PS nor ARM.
 //  The usage is simple:
-//   - on swithes 3:0 select the upper four bits of dividend, then press&release button 3
-//   - on swithes 3:0 select the lower four bits of dividend, then press&release button 2
-//   - on swithes 3:0 select the four bits of divisor, then press&release button 1
+//   - on switches 3:0 select the upper four bits of dividend, then press&release button 3
+//   - on switches 3:0 select the lower four bits of dividend, then press&release button 2
+//   - on switches 3:0 select the four bits of divisor, then press&release button 1
 //   - on LEDs 3:0 will be visible the four bits of the quotient
 //   - if you push the button 0, on the LEDs will appear the four bits of the remainder
 //////////////////////////////////////////////////////////////////////////////////
